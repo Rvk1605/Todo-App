5cde9c60435ff02da8ee4e4e6a56bf8052e8ecf2
Rajveer <1906351@kiit.ac.in>
2021-07-03 16:00:24 +0530

    Update Header.js

--- src/MyComponent/Header.js
+++ src/MyComponent/Header.js
@@ -6,7 +6,7 @@ export default function Header(props) {
     return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/"><h4>{props.title}</h4></Link>
+                    <Link className="navbar-brand" ><h4>TODO App</h4></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
